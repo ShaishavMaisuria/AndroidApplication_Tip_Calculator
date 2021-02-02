514b2b3a9e50dece669bce32b95aa3ea964c0ec2
ShaishavMaisuria <smaisuri@uncc.edu>
2021-02-01 20:40:57 -0500

    icon added

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         seekBar =findViewById(R.id.seekBar);
         textViewProgress=findViewById(R.id.textViewSeekBar);
         RadioGroup radioGroup =findViewById(R.id.radioGroup);
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -47,42 +48,60 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("demo","totalBilled");
                         totalAmount.setText(String.valueOf(totalbilled) + new String(" %"));
                     }catch (Exception e){
-                        Log.d(e.toString(),"10 catch");
+                        Log.d(e.toString(),"Radio button 10.0");
 
                     }
 
-                }else if(checkedId==R.id.radioButton15){
-                    totalbilled=Integer.parseInt(userValue.getText().toString())*1.15;
-                    totalAmount.setText(String.valueOf(totalbilled) + new String(" %"));
-                }else if(checkedId==R.id.radioButton18){
+                } if(checkedId==R.id.radioButton15){
+                    try {
+                        textViewTip.setText(String.valueOf("15.0 %"));
+
+                        totalbilled = Integer.parseInt(userValue.getText().toString()) * 1.15;
+                        totalAmount.setText(String.valueOf(totalbilled) + new String(" %"));
+                    }
+                    catch (Exception e){
+                        Log.d(e.toString(),"radio buttom 15.0");
+                    }
+                } if(checkedId==R.id.radioButton18){
+                  try{
+                    textViewTip.setText(String.valueOf("18.0 %"));
                     totalbilled=Integer.parseInt(userValue.getText().toString())*1.18;
                     totalAmount.setText(String.valueOf(totalbilled) + new String(" %"));
-                }else if(checkedId==R.id.radioButtonCustom){
-
-                    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                        @Override
-                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                            textViewProgress.setText(String.valueOf(progress)+new String(" %"));
-                            try {
-                                double tipValue = 1 + (progress *0.01);
-                                double total = Double.parseDouble(userValue.getText().toString()) * tipValue;
-                                totalAmount.setText(String.valueOf(total));
-                                textViewTip.setText(String.valueOf(progress) + new String(" %"));
-                            }catch(Exception e){
-                                Log.d(e.toString(),"10 catch");
+                }
+                    catch (Exception e){
+                        Log.d(e.toString(),"radio buttom 18.0");
+                }
+                } if(checkedId==R.id.radioButtonCustom){
+                    try {
+                        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                            @Override
+                            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                textViewProgress.setText(String.valueOf(progress) + new String(" %"));
+                                try {
+                                    double tipValue = 1 + (progress * 0.01);
+                                    double total = Double.parseDouble(userValue.getText().toString()) * tipValue;
+                                    totalAmount.setText(String.valueOf(total));
+                                    textViewTip.setText(String.valueOf(progress) + new String(" %"));
+                                } catch (Exception e) {
+                                    Log.d(e.toString(), "OnProgressChanged of seek bar catch");
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onStartTrackingTouch(SeekBar seekBar) {
+                            @Override
+                            public void onStartTrackingTouch(SeekBar seekBar) {
 
-                        }
+                            }
 
-                        @Override
-                        public void onStopTrackingTouch(SeekBar seekBar) {
+                            @Override
+                            public void onStopTrackingTouch(SeekBar seekBar) {
 
-                        }
-                    });
+                            }
+                        });
+
+                    }
+                    catch(Exception e){
+                        Log.d(e.toString(),"custom buttom");
+                    }
                 }
             }
         });
